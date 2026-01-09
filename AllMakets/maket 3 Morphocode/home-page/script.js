6baea322e80af2fe416607a1ad70cc39cbc1657b
KDVDimak <162099821+KDVDimak@users.noreply.github.com>
2026-01-09 16:24:20 +0200

    Add files via upload

---
+++ AllMakets/maket 3 Morphocode/home-page/script.js
@@ -0,0 +1,10 @@
+alert("dsadada"); 
+ 
+console.table({ name: "Dmitry", age: 33});
+
+const styles = "background-color: green";
+console.log("%cStart coding, now!",styles);
+
+let a ;
+a = "penis";
+console.log(a);
